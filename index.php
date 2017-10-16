61b671ed0febe41f29ff1fc4ec7b7bc0f37d0faf
Richard <solleer@hotmail.com>
2017-10-15 22:36:56 -0400

    Update axel classes and add caching, also use the MultJsonFileLoader on the index page

--- index.php
+++ index.php
@@ -6,14 +6,17 @@ session_start();
 
 date_default_timezone_set('America/New_York');
 
+// Setup cache for Axel
+$simpleCache = new \SimpleCache\SimpleCache('./tmp');
 // Set up Axel autoloader
-$axel = new \Axel\Axel;
-$axel->addModule(new \Axel\Module\PSR0('./Modules/'));
-$axel->addModule(new \Axel\Module\PSR0('./Config/', '\\Config'));
-$axel->addModule(new \Axel\Module\PSR0('./lib/'));
+$axel = new \Axel\Axel($simpleCache);
+$axel->addModule(new \Axel\Module\PSR4('./Modules/'));
+$axel->addModule(new \Axel\Module\Library($axel, './Modules/'));
+$axel->addModule(new \Axel\Module\PSR4('./Config/', '\\Config'));
+$axel->addModule(new \Axel\Module\PSR4('./lib/'));
 
 // Set up Dice
-$diceLoaderJson = new Dice\Loader\Json;
+$diceLoaderJson = new Config\Dice\MultJsonFileLoader(new Dice\Loader\Json);
 $dice = $diceLoaderJson->load('Config/Dice/Framework.json');
 $diceConfig = $dice->create('$config_dice');
 $dice = $diceLoaderJson->load($diceConfig->getFiles());
