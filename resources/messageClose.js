2e360ea75d015ac60d7d6a496c26dc62bc39207e
Richard <solleer@hotmail.com>
2018-12-04 12:43:24 -0500

    Change the way the close button works

--- resources/messageClose.js
+++ resources/messageClose.js
@@ -1,8 +1,9 @@
 $(function () {
-    var closeButton = $("<div class='o-pack__item'><i class='fa fas fa-times-circle fa-lg c-messages__close-btn' style='width: 1rem; cursor: pointer; float: right;'></i></div>")
-        .click(function () {
-            $(this).parent().remove();
-        });
+    var closeButton = $("<div class='o-pack__item'><i class='fa fas fa-times-circle fa-lg c-messages__close-btn' style='width: 1rem; cursor: pointer; float: right;'></i></div>");
 
     $(".c-messages__message").append(closeButton);
+
+    $(".c-messages").on("click", ".c-messages__close-btn", function () {
+        $(this).parent().parent().remove();
+    });
 });
