99ccbe728c2b5b9d56b660373c6ff1d7fbbd3008
Richard <solleer@hotmail.com>
2019-03-08 08:36:56 -0500

    Fix bug with loading Module config file

--- Config/Modules.php
+++ Config/Modules.php
@@ -18,12 +18,14 @@ class Modules {
     }
 
     private function getFile($file) {
-        if (!file_exists($file)) return false;
+        $fileName = __DIR__ . '/../' . $file;
+        if (!file_exists($fileName)) return false;
 
-        $settings = json_decode(str_replace('{dir}', dirname($file), file_get_contents($file)), true);
+        $settings = json_decode(str_replace('{dir}', dirname($file), file_get_contents($fileName)), true);
         if (isset($settings['extend'])) {
             $settings = array_merge($this->getFile($settings['extend']), $settings);
         }
         return $settings;
+
     }
 }
