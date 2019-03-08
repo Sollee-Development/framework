874953ee8ab460a64e4e84bc104c5765b80253df
Richard <solleer@hotmail.com>
2019-03-07 20:23:41 -0500

    Add dropdown menu capabilities

---
+++ resources/framework/dropdown-nav.js
@@ -0,0 +1,16 @@
+/* From w3schools */
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+$(function () {
+    $('.c-dropdown__btn').click(function () {
+        $(this).next(".c-dropdown__cont").toggle();
+    });
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.c-dropdown__btn')) {
+        $(".c-dropdown__cont").hide();
+    }
+}
