e4179e9753836bc20cbfc7c692af6a363deb7b86
Richard <solleer@hotmail.com>
2019-03-12 21:44:56 -0400

    Have hidding the dropdown use a class

--- resources/framework/dropdown-nav.js
+++ resources/framework/dropdown-nav.js
@@ -5,13 +5,13 @@ toggle between hiding and showing the dropdown content */
 $(function () {
     $('.c-dropdown__btn').click(function (event) {
         event.preventDefault();
-        $(this).nextAll(".c-dropdown__cont").toggle();
+        $(this).nextAll(".c-dropdown__cont").toggleClass('c-dropdown__cont--hide');
     });
 });
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
     if (!event.target.matches('.c-dropdown__btn')) {
-        $(".c-dropdown__cont").hide();
+        $(".c-dropdown__cont").addClass('c-dropdown__cont--hide');
     }
 }
