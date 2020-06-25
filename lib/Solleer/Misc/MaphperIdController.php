02ad34e9ef813a7da1f2ff043c878674c73bb24f
Richard <solleer@hotmail.com>
2020-06-25 10:24:20 -0400

    Give controller a better name

--- lib/Solleer/Misc/MaphperIdController.php
+++ lib/Solleer/Misc/MaphperIdController.php
@@ -6,8 +6,8 @@ namespace Solleer\Misc;
  * May move this to a library if I find more classes to package it with
  */
 
-class IdController {
-    public function id(\Maphper\Maphper $model, $id): stdClass {
+class MaphperIdController {
+    public function id(\Maphper\Maphper $model, $id): \stdClass {
         return $model[$id];
     }
 }
